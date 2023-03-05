a856f96043b55338919483266935e7a0dff2a9fc
Rithikesh <rithikeshsonu13@gmail.com>
2023-03-05 23:29:31 +0530

    HTML Helpers in MVC - Standard HTML Helpers

---
+++ HTMLHelperMVC/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLHelperMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Login(FormCollection values)
+        {
+            return View();
+        }
+    }
+}
