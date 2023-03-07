d5c73994bb148246c2c84ecc5daeb127ed5369fa
Rithikesh <rithikeshsonu13@gmail.com>
2023-03-07 23:27:01 +0530

    Strongly typed HTML Helpers

--- HTMLHelperMVC/Controllers/AccountController.cs
+++ HTMLHelperMVC/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HTMLHelperMVC.Models; //imported from Model class
 
 namespace HTMLHelperMVC.Controllers
 {
@@ -11,10 +12,21 @@ namespace HTMLHelperMVC.Controllers
         // GET: Account
         public ActionResult Login()
         {
+            ViewBag.username = "test";
             return View();
         }
         [HttpPost]
         public ViewResult Login(FormCollection values)
+        {
+            return View();
+        }
+        //Strongly typed HTML Helpers 
+        public ViewResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Register(RegisterViewModel model)
         {
             return View();
         }
---
+++ HTMLHelperMVC/Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HTMLHelperMVC.Models
+{
+    public class RegisterViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public char Gender { get; set; }
+        public string Address { get; set; }
+        public bool IsAgree { get; set; }
+    }
+}
